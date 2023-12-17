886779f7efc1d6abb47d3fb9b11ed0475031bbf6
BenLalor <lalor.benjamin@gmail.com>
2023-12-16 17:21:39 -0700

    Add SymbolSearch component - Add bootstrap - Add SymbolSearch component - Add graph components - update styling of rechart graphs

--- src/MonthlyGraph.js
+++ src/MonthlyGraph.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import {
-  LineChart,
-  Line,
   CartesianGrid,
   XAxis,
   YAxis,
   Tooltip,
   ResponsiveContainer,
+  AreaChart,
+  Area,
 } from "recharts";
 
 function MonthlyGraph({ preproccesedData }) {
@@ -31,19 +31,27 @@ function MonthlyGraph({ preproccesedData }) {
   return (
     <div className="App">
       <ResponsiveContainer width="100%" height={400}>
-        <LineChart
+        <AreaChart
           data={postProcessedData}
           margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
         >
-          <Line type="monotone" dataKey="close" stroke="#8884d8" />
-          <CartesianGrid stroke="#ccc" />
+          <defs>
+            <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="0%" stopColor="#2451B7" stopOpacity={0.4} />
+              <stop offset="75%" stopColor="#2451B7" stopOpacity={0.05} />
+            </linearGradient>
+          </defs>
+          <Area dataKey="close" stroke="#2451B7" fill="url(#color)" />
+          <CartesianGrid opacity={0.1} fillOpacity={0.1} vertical={false} />
           <XAxis dataKey="date" />
           <YAxis
+            tickCount={9}
+            axisLine={false}
             domain={["dataMin", "dataMax"]}
             tickFormatter={(value) => `$${value}`}
           />
           <Tooltip />
-        </LineChart>
+        </AreaChart>
       </ResponsiveContainer>
     </div>
   );
